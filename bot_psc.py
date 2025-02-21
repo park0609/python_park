28ca80368d12183bcf96d653046617ffbc1565d1
park0609 <guppy135@naver.com>
2025-02-21 17:21:44 +0900

    0221

--- bot_psc.py
+++ bot_psc.py
@@ -1,5 +1,6 @@
 from telegram import Update
 from telegram.ext import Applicate, CommandHandler, MwssageHandler, filters, CallbackContext
+import talk_db as tk
 
 TOKEN = "차단당해서 모름"
 
@@ -10,14 +11,15 @@ TRIGGER_WORDS = {
 
 async def start(update, context):
     await update.message.reply_text("안녕하세요 저는 차단당한 박세찬입니다. 좀 도와주세요")
+
 async def monitor_chat(update, context):
     user_text = update.message.text #감지된 메세지들
     chat_id = update.message.chat_id #메세지가 온 채팅방
     await
     
-    for key, res in TRIGGER_WORDS.items():
+    for key, res in tk.TRIGGER_WORDS.items():
         if key in user_text:
-            await context.send_message(chat_id=chat_id,text=res)
+            await context.bot.send_message(chat_id=chat_id,text=res)
             break #한개의 키워드에만 작용
 
     
